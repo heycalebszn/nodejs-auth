b7aa9d2b7e13b2bab5a60eb1ea8d0ed1768e5646
Raptomi <155694747+heycalebszn@users.noreply.github.com>
2024-12-15 11:42:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.json());
 app.use('/', router);
 app.use(cors());
 app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+    console.log('Server is running on port 4000')
 })
